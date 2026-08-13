6c269f59813090db6d56cab4c35e9c008c05b6db
yada898 <ay717933@gmail.com>
2026-08-13 14:57:16 +0530

    Time: 5 ms (18.77%) | Memory: 8.6 MB (36.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if(x<0) return false;
+        int n=x,rem;
+        long temp = 0;
+        while(n>0){
+            rem=n%10;
+            temp=temp*10+rem;
+            n/=10;
+        }
+        if(temp==x){
+            return true;
+        }
+        else{
             return false;
         }
-
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
-        }
-
-        // When length is odd, we can get rid of the middle digit by reversedHalf / 10.
-        // For example, for 12321: x = 12, reversedHalf = 123, so x == reversedHalf / 10 (12 == 12).
-        return x == reversedHalf || x == reversedHalf / 10;
-        
     }
 };
