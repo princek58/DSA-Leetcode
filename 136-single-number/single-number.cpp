7861084b8e8a24e0c6d2b6d49ee15d0fcf84c767
princek58 <ay717933@gmail.com>
2026-09-10 10:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (24.14%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int temp=0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            temp^=nums[i];
+        }
+        
+        return temp;
+        
+        
+        
+           }
+};
